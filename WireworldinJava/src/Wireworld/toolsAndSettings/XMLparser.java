5425ba50050fa098a978504ca56f897739b012bc
duszekxxxx <duszekxxxxxx@gmail.com>
2017-05-15 20:02:33 +0200

    Ending implementation of Logic

---
+++ WireworldinJava/src/Wireworld/toolsAndSettings/XMLparser.java
@@ -0,0 +1,89 @@
+
+package Wireworld.toolsAndSettings;
+
+import Wireworld.Logic.Board;
+import Wireworld.Logic.BoardGame;
+import Wireworld.Logic.Conductor;
+import Wireworld.Logic.ElectronHead;
+import Wireworld.Logic.ElectronTail;
+import Wireworld.Logic.EmptyCell;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class XMLparser {
+
+    public static BoardGame parser(String fileName) {
+        BoardGame board;
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(fileName);
+            NodeList nodeHorizontalSize = doc.getElementsByTagName("horizontalSize");
+            int horizontalSize = Integer.valueOf(nodeHorizontalSize.item(0).getTextContent());
+            NodeList nodeVerticalSize = doc.getElementsByTagName("verticalSize");
+            int verticalSize = Integer.valueOf(nodeHorizontalSize.item(0).getTextContent());
+            board = new Board(horizontalSize, verticalSize);
+            for (int k = 0; k < board.getVerticalSize(); k++) {
+                for (int m = 0; m < board.getHorizontalSize(); m++) {
+                    board.setPointOnBoard(new EmptyCell(), k, m);
+                }
+            }
+            NodeList Points = doc.getElementsByTagName("Points");
+            Node nPoints = Points.item(0);
+            NodeList States = nPoints.getChildNodes();
+            for (int i = 0; i < States.getLength(); i++) {
+                if (States.item(i).getNodeName().contentEquals("Conductor")) {
+                    NodeList Point = States.item(i).getChildNodes();
+                    for (int j = 0; j < Point.getLength(); j++) {
+
+                        if (Point.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                            Element ePoint = (Element) Point.item(j);
+                            int x = Integer.valueOf(ePoint.getAttribute("x"));
+                            int y = Integer.valueOf(ePoint.getAttribute("y"));
+                            board.setPointOnBoard(new Conductor(), x, y);
+                        }
+                    }
+                }
+                if (States.item(i).getNodeName().contentEquals("Head")) {
+                    NodeList Point = States.item(i).getChildNodes();
+                    for (int j = 0; j < Point.getLength(); j++) {
+
+                        if (Point.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                            Element ePoint = (Element) Point.item(j);
+                            int x = Integer.valueOf(ePoint.getAttribute("x"));
+                            int y = Integer.valueOf(ePoint.getAttribute("y"));
+                            board.setPointOnBoard(new ElectronHead(), x, y);
+                        }
+                    }
+                }
+                if (States.item(i).getNodeName().contentEquals("Tail")) {
+                    NodeList Point = States.item(i).getChildNodes();
+                    for (int j = 0; j < Point.getLength(); j++) {
+
+                        if (Point.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                            Element ePoint = (Element) Point.item(j);
+                            int x = Integer.valueOf(ePoint.getAttribute("x"));
+                            int y = Integer.valueOf(ePoint.getAttribute("y"));
+                            board.setPointOnBoard(new ElectronTail(), x, y);
+                        }
+                    }
+                }
+            }
+            return board;
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(XMLparser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
---
+++ WireworldinJava/src/Wireworld/toolsAndSettings/test.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Wireworld.toolsAndSettings;
+
+import Wireworld.Logic.Board;
+import Wireworld.Logic.BoardGame;
+import Wireworld.Logic.ElectronHead;
+import Wireworld.Logic.Conductor;
+import Wireworld.Logic.ElectronTail;
+import Wireworld.Logic.EmptyCell;
+
+/**
+ *
+ * @author duszekxxxx
+ */
+public class test {
+
+    public static void main(String argv[]) {
+
+        Board plansza = new Board(10, 10);
+        for (int i = 0; i < plansza.getHorizontalSize(); i++) {
+            for (int j = 0; j < plansza.getVerticalSize(); j++) {
+               plansza.setPointOnBoard(new EmptyCell(), i, j);
+            }
+        }
+        plansza.setPointOnBoard(new Conductor(), 5, 5);
+        for(int i =0;i<10;i++){
+          plansza.setPointOnBoard(new Conductor(), i, 7);  
+        }
+        plansza.setPointOnBoard(new ElectronHead(), 9, 9);
+        SaveAndOpenGeneration.saveToFile("generacja", plansza);
+        
+        BoardGame plansza2 = SaveAndOpenGeneration.openFile("generacja.xml");
+        System.out.println("Vertical Size: "+plansza2.getVerticalSize()+" Horizontal Size: "+plansza2.getHorizontalSize());
+        for (int i = 0; i < plansza2.getVerticalSize(); i++) {
+            for (int j = 0; j < plansza.getHorizontalSize(); j++) {
+                if (plansza2.getPointOnBoard(j, i) instanceof ElectronHead) {
+                    System.out.print("H");
+                } else if (plansza2.getPointOnBoard(j, i) instanceof ElectronTail) {
+                    System.out.print("T");
+                } else if (plansza2.getPointOnBoard(j, i) instanceof Conductor) {
+                    System.out.print("C");
+                } else if (plansza2.getPointOnBoard(j, i) instanceof EmptyCell) {
+                    System.out.print("B");
+                }
+            }
+            System.out.print("\n"); 
+        }    
+    }
+
+}
